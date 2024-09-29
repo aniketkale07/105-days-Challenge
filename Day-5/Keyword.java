8111fab84425a5af9e81e72d5f26339e6f86c4aa
aniketkale07 <codingwizard07@gmail.com>
2024-09-29 07:23:22 +0530

    Day 5

--- Day-5/Keyword.java
+++ Day-5/Keyword.java
@@ -1,18 +1,44 @@
 public class Keyword {
     public static void main(String[] args) {
-
-    }
-
-}
-
-class SuperClass {
-    SuperClass(int length, int breadth) {
-        System.out.println(length * breadth);
+        Cube cube = new Cube(4, 4, 2);
+        cube.display();
     }
 }
 
-class DerivedClass{
-    DerivedClass(int length , int breadth , int height){
-        
+class Rectangle {
+    private int length;
+    private int breadth;
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+        System.out.println("Area of rectangle: " + calculateArea());
+    }
+
+    private int calculateArea() {
+        return length * breadth;
+    }
+}
+
+class Cube extends Rectangle {
+    private int width;
+
+    public Cube(int length, int breadth, int width) {
+        super(length, breadth);
+        this.width = width;
+    }
+
+    public void display() {
+        System.out.println("This is a cube");
+        System.out.println("The area of the base square is: " + calculateBaseArea());
+        System.out.println("The volume of the cube is: " + calculateVolume());
+    }
+
+    private int calculateBaseArea() {
+        return width * width;
+    }
+
+    private int calculateVolume() {
+        return width * width * width;
     }
 }
