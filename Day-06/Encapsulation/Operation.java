85e6a949369c83a53488d559fb3b4a359a27258e
aniketkale07 <codingwizard07@gmail.com>
2024-10-08 07:15:07 +0530

    Day 6

---
+++ Day-06/Encapsulation/Operation.java
@@ -0,0 +1,15 @@
+ class Operation {
+
+    float cube(float side){
+        return side*side*side;
+    }
+
+    double circle(float r){
+        return 3.14*r*r;
+    }
+
+    int rectangle(int length , int breadth){
+        return length * breadth;
+    }
+    
+}
