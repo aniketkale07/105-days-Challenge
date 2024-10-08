8428a49e38ab8225b7264880b4d7cfbb488d316b
aniketkale07 <codingwizard07@gmail.com>
2024-10-08 07:15:19 +0530

    Day 7

--- Day-6/Encapsulation/Area.java
+++
@@ -1,9 +0,0 @@
-public class Area {
-    public static void main(String[] args) {
-        Operation operation = new Operation();
-
-        System.out.println("Area of Circle : "+operation.circle(5));
-        System.out.println("Area of Rectangle : "+operation.rectangle(5, 6));
-        System.out.println("Area of cube : "+operation.cube( 6.4f));  
-    }
-}
